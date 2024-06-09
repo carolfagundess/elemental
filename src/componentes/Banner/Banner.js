92e03f04008d45bcbcae3ecb6e36666a103e11ae
Carol Fagundes <carolinarftk@gmail.com>
2024-06-09 18:59:16 -0300

    primeiro componente: banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+  return (
+    <header className='banner'>
+        <img src="/imagens/banner.jpg" alt="A capa da página" />;
+    </header>
+  )
+}
+
+export default Banner;
