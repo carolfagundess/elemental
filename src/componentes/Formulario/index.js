48277dd6b5cbea4b3034abe894e1ffb21a7e642b
Carol Fagundes <carolinarftk@gmail.com>
2024-06-13 22:19:27 -0300

    criado componente listaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,15 +1,27 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 
 const Formulario = () => {
+  const clas = [
+    "Selecione um clã",
+    "Skywalker",
+    "Kenobi",
+    "Fett",
+    "Hutt",
+    "Mandalorian",
+    "Jedi",
+    "Sith",
+  ];
+
   return (
     <section className="formulario">
       <form>
         <h2>Preencha os dados para criar o card do Star Wars</h2>
-        <CampoTexto label="Personagem" placeholder="o nome do personagem" />
+        <CampoTexto label="Nome" placeholder="o nome do personagem" />
         <CampoTexto label="Ator" placeholder="o nome do ator" />
-        <CampoTexto label="Filme" placeholder="um filme deste personagem" />
         <CampoTexto label="Imagem" placeholder="o endereço da imagem" />
+        <ListaSuspensa label="Clãs" itens={clas} />
       </form>
     </section>
   );
