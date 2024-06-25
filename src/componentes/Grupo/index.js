c7c8ac5e1812ec10a07c1f797fa0c098390e499d
Carol Fagundes <carolinarftk@gmail.com>
2024-06-24 21:46:20 -0300

    criando input de cor

--- src/componentes/Grupo/index.js
+++ src/componentes/Grupo/index.js
@@ -5,15 +5,16 @@ import "./Grupo.css";
 const Grupo = (props) => {
   return props.personagens.length > 0 ? (
     <section className="grupo" style={{ backgroundColor: props.corSecundaria }}>
+      <input value={props.corPrimaria} type="color" className="input-cor" />
       <h3 style={{ borderBottomColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="personagens">
         {props.personagens.map((personagem) => (
           <Personagem
-            corDeFundo={props.corPrimaria}
             key={personagem.nome}
             nome={personagem.nome}
-            ator={personagem.ator}
             imagem={personagem.imagem}
+            ator={personagem.ator}
+            corDeFundo={props.corPrimaria}
             aoDeletar={props.aoDeletar}
           />
         ))}
