304836f6f260779747077a00495a822add75a686
Carol Fagundes <carolinarftk@gmail.com>
2024-06-25 21:19:21 -0300

    formulario para criar grupo

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -9,6 +9,9 @@ const Formulario = (props) => {
   const [ator, setAtor] = useState("");
   const [imagem, setImagem] = useState("");
   const [grupo, setGrupo] = useState("");
+  const [nomeGrupo, setNomeGrupo] = useState("");
+  const [GrupoCorPrimaria, setGrupoCorPrimaria] = useState("");
+  const [GrupoCorSecundaria, setGrupoCorSecundaria] = useState("");
 
   const salvar = (evento) => {
     evento.preventDefault();
@@ -58,6 +61,31 @@ const Formulario = (props) => {
           aoAlterar={(valor) => setGrupo(valor)}
         />
         <Botao>Criar card</Botao>
+      </form>
+      <form onSubmit={salvar}>
+        <h2>Preencha os dados para criar um novo grupo</h2>
+        <CampoTexto
+          obrigatorio
+          label="Nome"
+          placeholder="o nome do time"
+          valor={nome}
+          aoAlterar={(valor) => setNomeGrupo(valor)}
+        />
+        <CampoTexto
+          obrigatorio
+          label="Cor primária"
+          placeholder="a cor do grupo"
+          valor={ator}
+          aoAlterar={(valor) => setGrupoCorPrimaria(valor)}
+        />
+        <CampoTexto
+          obrigatorio
+          label="Cor secundária"
+          placeholder="a cor do grupo"
+          valor={ator}
+          aoAlterar={(valor) => setGrupoCorSecundaria(valor)}
+        />
+        <Botao>Criar um novo time</Botao>
       </form>
     </section>
   );
